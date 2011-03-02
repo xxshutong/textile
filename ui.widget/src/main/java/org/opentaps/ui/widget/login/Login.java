1c32fd67ed49397691912ff744b3e5ecab7bb4d4
Oleg Andreyev <oandreyev@opensourcestrategies.com>
2011-03-02 20:09:35 +0200

    Add a bundle for reusable UI fragments.

---
+++ ui.widget/src/main/java/org/opentaps/ui/widget/login/Login.java
@@ -0,0 +1,12 @@
+package org.opentaps.ui.widget.login;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class Login extends WebPage {
+
+    public Login(PageParameters parameters) {
+        super(parameters);
+    }
+
+}
