b6e05543e15d59ef7352a96facd036e08b8b17f7
Oleg Andreyev <oandreyev@opensourcestrategies.com>
2011-03-07 23:35:12 +0200

    Implement simple code for JAAS authentication.

--- webapp.poc.leads/src/main/java/org/opentaps/webapp/poc/leads/LeadsSession.java
+++ webapp.poc.leads/src/main/java/org/opentaps/webapp/poc/leads/LeadsSession.java
@@ -1,14 +1,36 @@
 package org.opentaps.webapp.poc.leads;
 
+import java.io.IOException;
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+
 import org.apache.wicket.Session;
 import org.apache.wicket.authroles.authentication.AuthenticatedWebSession;
 import org.apache.wicket.authroles.authorization.strategies.role.Roles;
 import org.apache.wicket.request.Request;
 
+import com.sun.security.auth.UserPrincipal;
+
 
 public class LeadsSession extends AuthenticatedWebSession {
 
     private static final long serialVersionUID = 6790743878501241576L;
+
+    public static final String REALM = "karaf";
+
+    String currentUser;
+    Set<String> roles;
+    Subject subject;
 
     public LeadsSession(Request request) {
         super(request);
@@ -17,28 +39,56 @@ public class LeadsSession extends AuthenticatedWebSession {
     @Override
     public boolean authenticate(final String username, final String password)
     {
-        final String USERNAME = "admin";
-        final String PASSWORD = "opentaps";
+        subject = new Subject();
+        LoginContext loginContext;
+        try {
+            loginContext = new LoginContext(REALM, subject, new CallbackHandler() {
 
-        return USERNAME.equals(username) && PASSWORD.equals(password);
+                public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+                    for (Callback callback : callbacks) {
+                        if (callback instanceof NameCallback) {
+                            ((NameCallback) callback).setName(username);
+                        } else if (callback instanceof PasswordCallback) {
+                            ((PasswordCallback) callback).setPassword(password != null ? password.toCharArray() : null);
+                        } else {
+                            throw new UnsupportedCallbackException(callback);
+                        }
+                    }
+                }
+                
+            });
+
+            loginContext.login();
+
+            for (Principal p : subject.getPrincipals()) {
+                if (p instanceof UserPrincipal) {
+                    currentUser = p.getName();
+                } else {
+                    if (roles == null) {
+                        roles = new HashSet<String>();
+                    }
+                    roles.add(p.getName());
+                }
+            }
+
+            return true;
+
+        } catch (LoginException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return false;
     }
 
     @Override
     public Roles getRoles()
     {
-        if (isSignedIn())
-        {
-            // If the user is signed in, they have these roles
-            return new Roles(Roles.ADMIN);
-        }
-        return null;
+        return (isSignedIn() && roles != null && !roles.isEmpty()) ? new Roles(roles.toArray(new String[0])) : null;
     }
 
     public static LeadsSession get() {
         return (LeadsSession) Session.get();
     }
 
-    public boolean isAuthenticated() {
-        return true;
-    }
 }
