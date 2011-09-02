40c53bd6fc166e600c63db79896b974a306897b4
Oleg Andreyev <oandreyev@opensourcestrategies.com>
2011-09-02 23:10:06 +0300

    Add a bundle from Orbit. Xml-rpc bundle depend on it.

--- thirdparty/install.sh
+++ thirdparty/install.sh
@@ -6,3 +6,10 @@
 # Source: Orbit project
 #
 mvn install:install-file -DgroupId=org.opentaps -DartifactId=org.apache.xmlrpc -Dversion=3.0.0-v20100427-1100 -Dpackaging=jar -Dfile=./lib/org.apache.xmlrpc_3.0.0.v20100427-1100.jar
+
+#
+# org.apache.ws.commons.util 1.0.1
+# Source: Orbit project
+#
+mvn install:install-file -DgroupId=org.opentaps -DartifactId=org.apache.ws.commons.util -Dversion=1.0.1-v20100518-1140 -Dpackaging=jar -Dfile=./lib/org.apache.ws.commons.util_1.0.1.v20100518-1140.jar
+
