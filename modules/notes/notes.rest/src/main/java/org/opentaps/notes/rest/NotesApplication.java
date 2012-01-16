a59367c6d97df1a867c17dc60344754ddc497fcf
Oleg Andreyev <oandreyev@opensourcestrategies.com>
2012-01-17 00:27:31 +0200

    #2595 Convert notes.rest bundle to Web Application Bundle project.

--- modules/notes/notes.rest/src/main/java/org/opentaps/notes/rest/NotesApplication.java
+++ modules/notes/notes.rest/src/main/java/org/opentaps/notes/rest/NotesApplication.java
@@ -7,12 +7,13 @@ import org.restlet.routing.Router;
 public class NotesApplication extends Application {
 
 	@Override
-	public Restlet createInboundRoot() {
+	public synchronized Restlet createInboundRoot() {
 		Router router = new Router(getContext());
 
 		// Attach the resources to the router
 		router.attach("/note", NoteResource.class);
         router.attach("/note/{noteId}", NoteResource.class);
+ 
 		return router;
 	}
 }
