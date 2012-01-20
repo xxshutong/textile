07a2b715b8a2e5f7ecd47118b56446e00e67c5a7
Jeremy Wickersheimer <jwickers@gmail.com>
2012-01-20 10:00:19 +0800

    Cleanup

--- modules/notes/api/services/src/main/java/org/opentaps/notes/services/GetNoteByIdService.java
+++ modules/notes/api/services/src/main/java/org/opentaps/notes/services/GetNoteByIdService.java
@@ -1,7 +1,5 @@
 package org.opentaps.notes.services;
 
-import org.opentaps.notes.domain.Note;
-
 public interface GetNoteByIdService {
 
     public GetNoteByIdServiceOutput getNoteById(GetNoteByIdServiceInput input);
