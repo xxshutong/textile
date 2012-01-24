4696ff90df6257154d7225e3d20afe7fc3a067b3
Jeremy Wickersheimer <jwickers@gmail.com>
2012-01-24 19:27:04 +0800

    Add tests for the validation
    Note that in the test we set the Apache implementation manually, in Geronimo this is all provided and JPA does the validation transparently.

---
+++ modules/notes/impl/tests/src/test/java/org/opentaps/notes/tests/BValValidationServiceResolver.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Open Source Strategies, Inc.
+ *
+ * Opentaps is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Opentaps is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Opentaps.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.opentaps.notes.tests;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.ValidationProviderResolver;
+import javax.validation.spi.ValidationProvider;
+
+/**
+ * A provider of the apache bval validation for bootstrapping Bean Validation in the tests.
+ * In geronimo the same implementation (apache bval) is provided by default
+ */
+public class BValValidationServiceResolver implements ValidationProviderResolver {
+
+
+    /** {@inheritDoc} */
+    public List<ValidationProvider<?>> getValidationProviders() {
+        List<ValidationProvider<?>> providers = new ArrayList<ValidationProvider<?>>();
+        providers.add(new org.apache.bval.jsr303.ApacheValidationProvider());
+        return providers;
+    }
+
+}
---
+++ modules/notes/impl/tests/src/test/java/org/opentaps/notes/tests/ValidationTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) Open Source Strategies, Inc.
+ *
+ * Opentaps is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Opentaps is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Opentaps.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.opentaps.notes.tests;
+
+
+import java.util.Set;
+import javax.validation.Configuration;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opentaps.notes.entity.NoteData;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+
+@RunWith(JUnit4TestRunner.class)
+public class ValidationTest extends NotesTestConfig {
+
+    private static Validator validator;
+
+    @BeforeClass
+    public static void setUpStatic() {
+        Configuration<?> config = Validation.byDefaultProvider()
+            .providerResolver(new BValValidationServiceResolver())
+            .configure();
+
+        ValidatorFactory factory = config.buildValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void testValidation() throws Exception {
+
+        NoteData note = new NoteData();
+        Set<ConstraintViolation<NoteData>> constraintViolations = validator.validate(note);
+        // ID, text, and created date time cannot be null
+        assertEquals(3, constraintViolations.size());
+    }
+}
