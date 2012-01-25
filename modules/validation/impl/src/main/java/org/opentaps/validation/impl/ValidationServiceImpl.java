bed4f7dd20d81423654ed9c0262e1485af21da10
Jeremy Wickersheimer <jwickers@gmail.com>
2012-01-25 21:28:35 +0800

    Add validation provider service
    The validation in JSR303 needs to be given a Validation Provider which normally works using the classpath but since we are using OSGI this can be problematic.
    In Geronimo the included implementation is already wired so Validation.buildDefaultValidatorFactory(); is enough, in other setups, like for example the unit tests, we have to specify the implementation hence ValidationServiceTestImpl which is hardcoded to find the apache bval implementation.
    
    By using an OSGI service ValidationService we can then have other modules access the validation in both contexts .

---
+++ modules/validation/impl/src/main/java/org/opentaps/validation/impl/ValidationServiceImpl.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Open Source Strategies, Inc.
+ *
+ * Opentaps is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Opentaps is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Opentaps.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.opentaps.validation.services.impl;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.opentaps.validation.services.ValidationService;
+
+
+public class ValidationServiceImpl implements ValidationService {
+
+    private ValidatorFactory factory;
+
+    public ValidationServiceImpl() {
+    }
+
+    public Validator getValidator() {
+        if (factory == null) {
+            synchronized(this) {
+                if (factory == null) {
+                    factory = Validation.buildDefaultValidatorFactory();
+                }
+            }
+        }
+
+        return factory.getValidator();
+    }
+
+}
