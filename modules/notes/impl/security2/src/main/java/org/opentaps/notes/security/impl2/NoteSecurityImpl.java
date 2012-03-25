295b2f5ad226180f96398cb25c027278832c30f3
Oleg Andreyev <oandreyev@opensourcestrategies.com>
2012-03-25 18:31:01 +0300

    Add license notice

--- modules/notes/impl/security2/src/main/java/org/opentaps/notes/security/impl2/NoteSecurityImpl.java
+++ modules/notes/impl/security2/src/main/java/org/opentaps/notes/security/impl2/NoteSecurityImpl.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright (c) Open Source Strategies, Inc.
+ *
+ * Opentaps is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Opentaps is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Opentaps.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.opentaps.notes.security.impl2;
 
+import org.opentaps.core.log.Log;
 import org.opentaps.notes.domain.Note;
 import org.opentaps.notes.security.NoteSecurity;
-import org.opentaps.notes.security.NoteSecurity.Operation;
-import org.opentaps.core.log.Log;
 
 public class NoteSecurityImpl implements NoteSecurity {
 
